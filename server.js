ab27a4beb6505120b8f7ea9ccdab96573c0a0fb9
lazySia <tldk9640@naver.com>
2021-11-28 23:10:55 +0900

    html파일 띄우기

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+
+app.get("/pet", function (요청, 응답) {
+  응답.send("펫 용품 쇼핑 페이지입니다.");
+});
+
+app.get("/beauty", function (요청, 응답) {
+  응답.send("뷰티 용품 쇼핑 페이지입니다.");
+});
+
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
