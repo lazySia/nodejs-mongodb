aadc3354372bbb9ced3cacd61b5475f3b842642f
lazySia <tldk9640@naver.com>
2021-11-30 23:42:54 +0900

    ejs
    ejs db 데이터 html로 보기

--- server.js
+++ server.js
@@ -2,30 +2,43 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
 const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
+
+var db;
 MongoClient.connect("mongodb+srv://admin:qwer1234@cluster0.ka8sc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", function (에러, client) {
+  if (에러) {
+    return console.log(에러);
+  }
+  db = client.db("todoapp");
+
+  //   db.collection("post").insertOne({ 이름: "John", _id: 100 }, function (에러, 결과) {
+  //     console.log("저장완료");
+  //   });
+
+  app.get("/write", function (요청, 응답) {
+    응답.sendFile(__dirname + "/write.html");
+  });
+
+  app.post("/add", function (요청, 응답) {
+    응답.send("전송완료");
+    console.log(요청.body.date);
+    console.log(요청.body.title);
+    db.collection("post").insertOne({ 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
+      console.log("저장완료");
+    });
+  });
+
   app.listen(8080, () => {
     console.log("listening on 8080");
   });
 });
 
-app.get("/pet", (요청, 응답) => {
-  응답.send("펫 용품 쇼핑 페이지입니다.");
-});
-
-app.get("/beauty", (요청, 응답) => {
-  응답.send("뷰티 용품 쇼핑 페이지입니다.");
-});
-
-app.get("/", (요청, 응답) => {
-  응답.sendFile(__dirname + "/index.html");
-});
-
-app.get("/write", (요청, 응답) => {
-  응답.sendFile(__dirname + "/write.html");
-});
-
-app.post("/add", (요청, 응답) => {
-  응답.send("전송완료");
+app.get("/list", function (요청, 응답) {
+  db.collection("post")
+    .find()
+    .toArray(function (에러, 결과) {
+      console.log(결과);
+      응답.render("list.ejs", { posts: 결과 });
+    });
 });
