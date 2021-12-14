c63a525409a69ccc83c3378501dae953dccb9c90
lazySia <tldk9640@naver.com>
2021-12-14 13:33:35 +0900

    mypage

--- server.js
+++ server.js
@@ -112,6 +112,21 @@ app.get("/fail", function (요청, 응답) {
   응답.send("로그인실패");
 });
 
+//로그인했니 함수 미들웨어 사용
+app.get("/mypage", 로그인했니, function (요청, 응답) {
+  // 요청.user;
+  응답.render("mypage.ejs", { data: 요청.user });
+});
+
+function 로그인했니(요청, 응답, next) {
+  if (요청.user) {
+    //요청.user 있습니까?
+    next();
+  } else {
+    응답.send("로그인안하셨는데요?");
+  }
+}
+
 // 아이디 비번 인증하는 세부 코드
 passport.use(
   new LocalStrategy(
@@ -121,14 +136,17 @@ passport.use(
       session: true, // 로그인 후 세션을 저장할 것인지
       passReqToCallback: false,
     },
+    // 아이디/비번 맞는지 DB와 비교
     function (입력한아이디, 입력한비번, done) {
-      //console.log(입력한아이디, 입력한비번);
+      // console.log(입력한아이디, 입력한비번);
       db.collection("login").findOne({ id: 입력한아이디 }, function (에러, 결과) {
         if (에러) return done(에러);
-
+        // DB에 아무것도 없을 때
         if (!결과) return done(null, false, { message: "존재하지않는 아이디요" });
+        // DB에 있을 때
         if (입력한비번 == 결과.pw) {
-          return done(null, 결과);
+          // 해쉬함수 등으로 암호화 해서 비교해야함
+          return done(null, 결과); // 라이브러리 문법:3개의 파라미터(서버에러, 성공시사용자DB데이터, 에러메세지)
         } else {
           return done(null, false, { message: "비번틀렸어요" });
         }
@@ -136,3 +154,16 @@ passport.use(
     }
   )
 );
+
+passport.serializeUser(function (user, done) {
+  //id 이용해서 세션을 저장시키는 코드
+  done(null, user.id);
+});
+passport.deserializeUser(function (아이디, done) {
+  // 마이페이지 접속시 발동. 나중에 쓸거임. 이 세션 데이터 가진 사람을 DB에서 찾아주세요
+  // 로그인한 유저의 개인정보를 DB에서 찾는 역할
+  //디비에서 위의 user.id로 유저를 찾은 뒤 유저 정보를 넣을것임
+  db.collection("login").findOne({ id: 아이디 }, function (에러, 결과) {
+    done(null, 결과); // {id:test, pw:test}
+  });
+});
