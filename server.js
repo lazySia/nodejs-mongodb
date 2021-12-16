99451caf0a7f66947ea87bb850e7aca21d77caa6
lazySia <tldk9640@naver.com>
2021-12-16 09:48:00 +0900

    image multer

--- server.js
+++ server.js
@@ -206,3 +206,41 @@ app.delete("/delete", function (요청, 응답) {
 //고객이 이 경로로 요청했을 때 이런 미들웨어(방금 만든 라우터) 적용해주세요!
 app.use("/shop", require("./routes/shop.js"));
 app.use("/board/sub", require("./routes/board.js"));
+
+app.get("/upload", function (요청, 응답) {
+  응답.render("upload.ejs");
+});
+
+let multer = require("multer");
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/image");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var path = require("path");
+
+var upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      return callback(new Error("PNG, JPG만 업로드하세요"));
+    }
+    callback(null, true);
+  },
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+});
+
+app.post("/upload", upload.single("프로필"), function (요청, 응답) {
+  응답.send("업로드완료");
+});
+
+app.get("/image/:imageName", function (요청, 응답) {
+  응답.sendFile(__dirname + "/public/image/" + 요청.params.imageName);
+});
